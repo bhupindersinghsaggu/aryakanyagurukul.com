929ea432ba5ed80b30fe9992888267da5dc6fec5
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-11-29 14:29:28 +0530

    Update header.php

--- akgcos/_web/header.php
+++ akgcos/_web/header.php
@@ -147,8 +147,7 @@
                                         <li class="nav-item dropdown">
                                             <a class="nav-link" href=#">Admission</a>
                                             <ul class="dropdown-menu">
-                                            
-                                            <li><a href="images/courses-eligibility.pdf" class="nav-link">Fee Flow</a></li>
+                                            <li><a href="images/courses-eligibility.pdf" class="nav-link">Courses Eligibility</a></li>
                                             <li><a href="images/fee-flow.pdf" class="nav-link">Fee Flow</a></li>
                                             <li><a href="images/fee.pdf" class="nav-link">Fees Details (2024-25) </a>
                                             </li>
