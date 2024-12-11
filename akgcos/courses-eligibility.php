24ca32baf1b033509aa8d6772e71b6662d9a9075
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-12-11 11:52:28 +0530

    Update courses-eligibility.php

--- akgcos/courses-eligibility.php
+++ akgcos/courses-eligibility.php
@@ -3,7 +3,50 @@
 include('_web/header.php')
 ?>
 
+<?php
 
+include('_web/header.php')
+?>
+
+<section class="page-title">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="theme-breadcrumb-box">
+                    <h1>Courses Eligibility</h1>
+
+                </div>
+            </div>
+        </div>
+        <!-- / .row -->
+    </div>
+    <!-- / .container -->
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-12 ps-lg-10 order-lg-1">
+            <div class="table-heading">
+                <h3> Arya Kanya Gurukul Sanskrit Mahavidyalaya Mor Majra (Karnal)</h3>
+                <h4>Courses Offered</h4>
+                <ul><li>Visharad</li>
+                <li>Shastri</li></ul>
+                <h5>Eligibility of Courses:</h5>
+                
+                <div><strong>VISHARAD</strong></div>
+                <p>विशारद परीक्षा में प्रवेश लेने के लिए दसवीं कक्षा उत्तीर्ण करना अनिवार्य हैI दसवीं कक्षा में अगर संस्कृत विषय नहीं है तो भी आप विशारद में प्रवेश ले सकते हैंI विशारद परीक्षा का संपूर्ण पाठ्यक्रम दो वर्ष का होगा, इसमें संस्कृत विषय अनिवार्य होगा। संस्कृतेतर विषयों का पाठ्यक्रम हरियाणा स्कूल शिक्षा बोर्ड क्रमशः (हिन्दी/अंग्रेजी) एकादश द्वादश कक्षाओं के समकक्ष होगा। विशारद कुरुक्षेत्र विश्वविद्यालय ,कुरुक्षेत्र से मान्यता प्राप्त है।</p>
+                <div><strong>SHASTRI</strong></div>
+                <p>शास्त्री प्रथम वर्ष में प्रवेश लेने के लिए 12वीं कक्षा उत्तीर्ण करना अनिवार्य है। 12वीं कक्षा में अगर संस्कृत विषय नहीं है तो भी आप शास्त्री में प्रवेश ले सकते हैं ।शास्त्री का संपूर्ण पाठ्यक्रम NEP 2020 के तहत 3 वर्षीय (6 सेमेस्टर) का होगा, शास्त्री के तीन विषय संस्कृत भाषा में हैं और अन्य बी.ए. के समकक्ष हैI शास्त्री कुरुक्षेत्र विश्वविद्यालय ,कुरुक्षेत्र से मान्यता प्राप्त है।</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+
+include('_web/footer.php')
+?>
 
 
 <?php
