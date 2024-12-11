1c2891dac83c49080cf5287e5d5f5f3473b71173
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-12-11 11:37:25 +0530

    Update footer.php

--- akgcos/_web/footer.php
+++ akgcos/_web/footer.php
@@ -32,18 +32,18 @@
         </ul>
       </div>
       <div class="col-lg-4 col-md-6 mt-5 mt-lg-0">
-        <h5 class="mb-4">Reach us</h5>
+        <h5 class="mb-4">Contact us</h5>
         <div class="footer-cntct">
           <ul class="media-icon list-unstyled">
             <li>
               <i class="flaticon-paper-plane"></i>
               <p class="mb-0">
-                आर्य कन्या गुरुकुल संस्कृत महाविद्यालय
+              Arya Kanya Gurukul Sanskrit Mahavidyalaya, Mor Majra (Karnal) 132046
               </p>
             </li>
             <li>
               <i class="flaticon-message"></i>
-              <a href="mailto:globus21in@yahoo.co.in">akmmgknl@gmail.com</a>
+              <a href="mailto:akmmgknl@gmail.com">akmmgknl@gmail.com</a>
             </li>
             <li>
               <i class="flaticon-phone-call"></i>
@@ -54,11 +54,6 @@
       </div>
       
       <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
-        <h5 class="mb-4">Contact us</h5>
-        <ul class="media-icon list-unstyled txt-white">
-        <li class="mb-3 "> Arya Kanya Gurukul Sanskrit Mahavidyalaya, Mor Majra (Karnal) 132046
-        </ul>
-     
         <h5 class="mb-4">Locate us</h5>
         <ul class="media-icon list-unstyled txt-white">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6947.24297745152!2d76.78581259343524!3d29.46906125087305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390de04855555555%3A0xdf5c3422e4ab695e!2sArya%20Kanya%20Gurukul%20College%20Of%20Education!5e0!3m2!1sen!2sin!4v1733897081255!5m2!1sen!2sin" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
