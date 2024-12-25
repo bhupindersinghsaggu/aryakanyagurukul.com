5c71d31aba3f6cd8dd3be590decc1efb9fa40558
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-12-25 16:15:59 +0530

    Update footer.php

--- akgm/_web/footer.php
+++ akgm/_web/footer.php
@@ -1,103 +1,109 @@
 <div class="footer-area pt-100 pb-70">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-sm-6">
-                    <div class="footer-widjet">
-                        <h3>About us</h3>
-                        <p style="color: #fff;line-height: 29px;">Arya Kanya Mahavidyalaya Gurukul (AKMG), a girls
-                            college affiliated to Kurukshetra University, Kurukshetra was established in 2008. The
-                            President of AKMG, aims to make quality education accessible to girl students from the
-                            economically and educationally disadvantaged community of rural areas. </p>
-                             
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <div class="footer-widjet">
+                    <h3>About us</h3>
+                    <p style="color: #fff;line-height: 29px;">Arya Kanya Mahavidyalaya Gurukul (AKMG), a girls
+                        college affiliated to Kurukshetra University, Kurukshetra was established in 2008. The
+                        President of AKMG, aims to make quality education accessible to girl students from the
+                        economically and educationally disadvantaged community of rural areas. </p>
 
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="footer-widjet">
+                    <h3>Important Links</h3>
+                    <div class="list">
+                        <ul>
+                            <li><a href="history.php">History</a></li>
+                            <li>
+                                <a href="assets/images/fee-flow.pdf" class="nav-link">Payment Flow</a>
+                            </li>
+                            <li><a href="fees.pdf">Fee Structure</a></li>
+                            <li><a href="https://admissions.highereduhry.ac.in/">Apply Online</a></li>
+                            <li><a href="IDP.php">Institutional Development Plan (IDP)
+                                    PLAN (IDP)</a></li>
+                            <li><a href="assets/images/pdf/nep.pdf">National Education Policy (NEP)</a></li>
+                            <li><a href="annual-reports.php">Annual Reports</a></li>
+                            <li><a href="terms-conditions.php">Terms & Conditions</a></li>
+
+                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="footer-widjet">
-                        <h3>Important Links</h3>
-                        <div class="list">
-                            <ul>
-                                <li><a href="history.php">History</a></li>
-                                <li><a href="fee.pdf">Fee Structure</a></li>
-                                <li><a href="https://admissions.highereduhry.ac.in/">Apply Online</a></li>
-                                <li><a href="IDP.php">Institutional Development Plan (IDP)
-                                        PLAN (IDP)</a></li>
-                                <li><a href="assets/images/pdf/nep.pdf">National Education Policy (NEP)</a></li>
-                                <li><a href="annual-reports.php">Annual Reports</a></li>
-                                 <li><a href="terms-conditions.php">Terms & Conditions</a></li>
-                               
-                            </ul>
-                        </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="footer-widjet">
+                    <h3>Courses</h3>
+                    <div class="list">
+                        <ul>
+                            <li><a href="arts-Humanities.php">B.A (Arts)</a></li>
+                            <li><a href="arts-Humanities.php">M.A (English)</a></li>
+                            <li><a href="arts-Humanities.php">M.A
+                                    (Sanskrit)</a></li>
+                            <li><a href="commerce-management.php"> B.Com.</a></li>
+                            <li><a href="commerce-management.php">B.B.A.</a></li>
+                            <li><a href="commerce-management.php">M.Com</a></li>
+                            <li><a href="physical-science.php">Bachelor of Physical Science</a></li>
+                            <li><a href="computer-application.php"> Computer Application</a></li>
+
+                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="footer-widjet">
-                        <h3>Courses</h3>
-                        <div class="list">
-                            <ul>
-                                <li><a href="arts-Humanities.php">B.A (Arts)</a></li>
-                                <li><a href="arts-Humanities.php">M.A (English)</a></li>
-                                <li><a href="arts-Humanities.php">M.A
-                                        (Sanskrit)</a></li>
-                                <li><a href="commerce-management.php"> B.Com.</a></li>
-                                <li><a href="commerce-management.php">B.B.A.</a></li>
-                                <li><a href="commerce-management.php">M.Com</a></li>
-                                <li><a href="physical-science.php">Bachelor of Physical Science</a></li>
-                                <li><a href="computer-application.php"> Computer Application</a></li>
+            </div>
+            <div class="col-lg-2 col-sm-6">
+                <div class="footer-widjet">
 
-                            </ul>
-                        </div>
+                    <h3>Contact us</h3>
+                    <div class="list">
+                        <ul>
+                            <li><a href="#">Arya Kanya Mahavidyalaya Gurukul, MorMajra (Karnal) 132046</a>
+                            </li>
+                            <li><a href="#">akmmgknl@gmail.com</a></li>
+                            <li><a href="#">8222000902, 8222000827</a></li>
+                        </ul>
                     </div>
-                </div>
-                <div class="col-lg-2 col-sm-6">
-                    <div class="footer-widjet">
-                        <br>
-                        <h3>Contact us</h3>
-                        <div class="list">
-                            <ul>
-                                <li><a href="#">Arya Kanya Mahavidyalaya Gurukul,MorMajra (Karnal) 132046</a>
-                                </li>
-                                <li><a href="#">akmmgknl@gmail.com</a></li>
-                                <li><a href="#">8222000902, 8222000827</a></li>
-                            </ul>
-                        </div>
-                        <br>
-                        <h3>Locate us</h3>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6947.24297745152!2d76.78581259343524!3d29.46906125087305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390de04855555555%3A0xdf5c3422e4ab695e!2sArya%20Kanya%20Gurukul%20College%20Of%20Education!5e0!3m2!1sen!2sin!4v1733897081255!5m2!1sen!2sin" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <br>
+                    <h3>Locate us</h3>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6947.24297745152!2d76.78581259343524!3d29.46906125087305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390de04855555555%3A0xdf5c3422e4ab695e!2sArya%20Kanya%20Gurukul%20College%20Of%20Education!5e0!3m2!1sen!2sin!4v1733897081255!5m2!1sen!2sin"
+                        width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="go-top">
-        <i class="ri-arrow-up-s-line"></i>
-        <i class="ri-arrow-up-s-line"></i>
-    </div>
-    <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
-    <script src="assets/js/jquery.min.js"></script>
+</div>
+<div class="go-top">
+    <i class="ri-arrow-up-s-line"></i>
+    <i class="ri-arrow-up-s-line"></i>
+</div>
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
 
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
 
-    <script src="assets/js/jquery.meanmenu.js"></script>
+<script src="assets/js/jquery.meanmenu.js"></script>
 
-    <script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
 
-    <script src="assets/js/carousel-thumbs.min.js"></script>
+<script src="assets/js/carousel-thumbs.min.js"></script>
 
-    <script src="assets/js/jquery.magnific-popup.js"></script>
+<script src="assets/js/jquery.magnific-popup.js"></script>
 
-    <script src="assets/js/aos.js"></script>
+<script src="assets/js/aos.js"></script>
 
-    <script src="assets/js/odometer.min.js"></script>
+<script src="assets/js/odometer.min.js"></script>
 
-    <script src="assets/js/appear.min.js"></script>
+<script src="assets/js/appear.min.js"></script>
 
-    <script src="assets/js/form-validator.min.js"></script>
+<script src="assets/js/form-validator.min.js"></script>
 
-    <script src="assets/js/contact-form-script.js"></script>
+<script src="assets/js/contact-form-script.js"></script>
 
-    <script src="assets/js/ajaxchimp.min.js"></script>
+<script src="assets/js/ajaxchimp.min.js"></script>
 
-    <script src="assets/js/custom.js"></script>
+<script src="assets/js/custom.js"></script>
 </body>
