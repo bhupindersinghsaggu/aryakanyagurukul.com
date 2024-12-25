cbe0d6534c74b85a0dc8d4cfbc9ef1c7c17ad5f2
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-12-25 16:46:52 +0530

    Update footer.php

--- akgcos/_web/footer.php
+++ akgcos/_web/footer.php
@@ -1,72 +1,78 @@
 <footer class="dark-bg footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-3 col-md-6">
-        <h5 class="mb-4">About School</h5>
-        <p style="color: #fff;">आर्य कन्या गुरुकुल मोर माजरा,
-          असन्ध वाया गोली राज मार्ग पर स्थित पवित्र-सुन्दर- प्राकृतिक
-          गाँव मोर माजरा से दो किलोमीटर पूर्व में मुख्य सड़क पर स्थित
-          है। पढाई को नियमित और सुचारु रूप से चलाने के लिए
-          आचार्या निर्मला आर्या के अथक प्रयास एवं समिति के सहयोग से
-          संस्था में 1989 में आर्य कन्या गुरुकुल संस्कृत महाविद्यालय की
-          स्थापना करके कुरुक्षेत्र विश्वविद्यालय से मान्यता प्राप्त की |</p>
-      </div>
-      <div class="col-lg-2 col-md-6 mt-5 mt-md-0">
-        <h5 class="mb-4">More Link</h5>
-        <ul class="list-unstyled mb-0 footer-menu">
-          <li class="mb-3">
-            <a href="about-college.php">About Us</a>
-          </li>
-          <li class="mb-3">
-            <a href="governing-body.php">Mangement</a>
-          </li>
-          <li class="mb-3">
-            <a href="faculty.php">Faculty</a>
-          </li>
-          <li class="mb-3">
-            <a href="academics.php">Academics</a>
-          </li>
-          <li class="mb-3">
-            <a href="terms-conditions.php">Terms & Conditions</a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-lg-4 col-md-6 mt-5 mt-lg-0">
-        <h5 class="mb-4">Contact us</h5>
-        <div class="footer-cntct">
-          <ul class="media-icon list-unstyled">
-            <li>
-              <i class="flaticon-paper-plane"></i>
-              <p class="mb-0">
-              Arya Kanya Gurukul Sanskrit Mahavidyalaya, Mor Majra (Karnal) 132046
-              </p>
-            </li>
-            <li>
-              <i class="flaticon-message"></i>
-              <a href="mailto:akmmgknl@gmail.com">akmmgknl@gmail.com</a>
-            </li>
-            <li>
-              <i class="flaticon-phone-call"></i>
-              <a href="tel:+8222000902">8222000902, 8222000827</a>
-            </li>
-          </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <h5 class="mb-4">About School</h5>
+                <p style="color: #fff;">आर्य कन्या गुरुकुल मोर माजरा,
+                    असन्ध वाया गोली राज मार्ग पर स्थित पवित्र-सुन्दर- प्राकृतिक
+                    गाँव मोर माजरा से दो किलोमीटर पूर्व में मुख्य सड़क पर स्थित
+                    है। पढाई को नियमित और सुचारु रूप से चलाने के लिए
+                    आचार्या निर्मला आर्या के अथक प्रयास एवं समिति के सहयोग से
+                    संस्था में 1989 में आर्य कन्या गुरुकुल संस्कृत महाविद्यालय की
+                    स्थापना करके कुरुक्षेत्र विश्वविद्यालय से मान्यता प्राप्त की |</p>
+            </div>
+            <div class="col-lg-2 col-md-6 mt-5 mt-md-0">
+                <h5 class="mb-4">More Link</h5>
+                <ul class="list-unstyled mb-0 footer-menu">
+                    <li class="mb-3">
+                        <a href="about-college.php">About Us</a>
+                    </li>
+                    <li class="mb-3">
+                        <a href="governing-body.php">Mangement</a>
+                    </li>
+                    <li class="mb-3">
+                        <a href="faculty.php">Faculty</a>
+                    </li>
+                    <li class="mb-3">
+                        <a href="academics.php">Academics</a>
+                    </li>
+                    <li class="mb-3">
+                        <a href="fees.php">Fees Details</a>
+                    </li>
+                    <li class="mb-3">
+                        <a href="terms-conditions.php">Terms & Conditions</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-5 mt-lg-0">
+                <h5 class="mb-4">Contact us</h5>
+                <div class="footer-cntct">
+                    <ul class="media-icon list-unstyled">
+                        <li>
+                            <i class="flaticon-paper-plane"></i>
+                            <p class="mb-0">
+                                Arya Kanya Gurukul Sanskrit Mahavidyalaya, Mor Majra (Karnal) 132046
+                            </p>
+                        </li>
+                        <li>
+                            <i class="flaticon-message"></i>
+                            <a href="mailto:akmmgknl@gmail.com">akmmgknl@gmail.com</a>
+                        </li>
+                        <li>
+                            <i class="flaticon-phone-call"></i>
+                            <a href="tel:+8222000902">8222000902, 8222000827</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                <h5 class="mb-4">Locate us</h5>
+                <ul class="media-icon list-unstyled txt-white">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6947.24297745152!2d76.78581259343524!3d29.46906125087305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390de04855555555%3A0xdf5c3422e4ab695e!2sArya%20Kanya%20Gurukul%20College%20Of%20Education!5e0!3m2!1sen!2sin!4v1733897081255!5m2!1sen!2sin"
+                        width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </ul>
+            </div>
         </div>
-      </div>
-      
-      <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
-        <h5 class="mb-4">Locate us</h5>
-        <ul class="media-icon list-unstyled txt-white">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6947.24297745152!2d76.78581259343524!3d29.46906125087305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390de04855555555%3A0xdf5c3422e4ab695e!2sArya%20Kanya%20Gurukul%20College%20Of%20Education!5e0!3m2!1sen!2sin!4v1733897081255!5m2!1sen!2sin" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </ul>
-      </div>
     </div>
-  </div>
 </footer>
 <!--footer end-->
 </div>
 
 <div class="scroll-top">
-  <a class="smoothscroll" href="#top"><i class="las la-arrow-up"></i></a>
+    <a class="smoothscroll" href="#top"><i class="las la-arrow-up"></i></a>
 </div>
 
 <!--back-to-top end-->
