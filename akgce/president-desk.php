4a3432de74daffc3b3f2c36bb6b53a8d20d6e42f
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-03-10 13:28:51 +0530

    Update president-desk.php

--- akgce/president-desk.php
+++ akgce/president-desk.php
@@ -31,62 +31,46 @@ include('_web/header.php');
       </div>
     </section>
     <section class="team-area-content">
-      <div class="container">
-        <!-- Lower Content -->
-        <div class="lower-content">
-          <div class="row">
-            <div class="text-column col-lg-7 col-md-12 col-sm-12">
-              <div
-                class="s-about-content pl-30 wow fadeInRight"
-                data-animation="fadeInRight"
-                data-delay=".2s"
-                style="visibility: visible; animation-name: fadeInRight"
-              >
-                <h2>Message From President</h2>
+        <div class="container">
+            <!-- Lower Content -->
+            <div class="lower-content">
+                <div class="row">
+                    <div class="text-column col-lg-7 col-md-12 col-sm-12">
+                        <div class="s-about-content pl-30 wow fadeInRight" data-animation="fadeInRight" data-delay=".2s" style="visibility: visible; animation-name: fadeInRight">
+                            <h2>Message From President</h2>
+                            <p>Dear students, Parents and Esteemed Faculty,</p>
+                            <p>
+                                It is with great joy and pride that I welcome you to Arya Kanya Gurukul, a place of learning, growth and excellence.
 
-                <p>
-                                An Educational Institute like Gurukul is a ‘World in miniature’ where
-                                one receives ‘training for life’, where effective, meaningful and joyful
-                                learning take place. Our Gurukul is a place where our life’s lessons
-                                are learnt-to accept challenges, face competition, defeat and failure,
-                                rejoice at victory and triumph. Since 2008, Arya Kanya
-                                Gurukul,College of Education,Mor Majra,Karnal has been serving the
-                                society by providing high quality of education at the affordable cost.
-                                Arya Kanya Gurukul, Mor Majra, has successfully Completed its
-                                illustrious 50 year journey by making a number of accomplishments.
+                                At our Gurukul believe in nurturing young minds nith knowledge, discipline and values that shape responsible citizens. Our mission is to empower girls with skills, wisdom and confidence to excel in all walks of life .Remember education is not just about acquiring knowledge but about developing a mindset of curiosity. Believe in yourself, work hard and success will follow.
                             </p>
                             <p>
-                                The goal of the Gurulkul education system is to fully develop the girl
-                                pupils so that they are capable of working and thinking critically. In
-                                the last 50 years, Gurukul has produced unheard of advancement in
-                                the field of education, culture, sports and other activities. Gurukul is
-                                the better and safe place for girl’s students to begin their career.
-                                Our Management committee express the gratitude to the parents for
-                                their unstinted support, which helps make the dreams of our
-                                students come true.
-                            </p>
-                            <p>
-                                “Education is shared commitment between dedicated teachers,
-                                motivated students and enthusiastic parents with high expectations”.
+                            As we move forward let us work together to make Gurukul a place of inspiration and excellence. Wishing you all a year filled with learning, growth and success.
                             </p>
                             <div class="prev-link">
-                                <h4><a href="#">Advocate Jasbir Singh Maan</a></h4>
+                                <h4><a href="#">Sukhbir Singh</a></h4>
                                 <span>Arya Kanya Mahasabha Gurukul,
                                     Mor Majra,Karnal </span>
                             </div>
-                
-              </div>
+                            <div class="two-column mt-30">
+                                <div class="row aling-items-center">
+                                    <div class="image-column col-xl-6 col-lg-12 col-md-12">
+                                        <div class="slider-btn">
+                                            <a href="president-desk.php" class="btn ss-btn smoth-scroll">Read More <i class="fal fa-long-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-12 col-sm-12">
+                        <div class="team-img-box">
+                            <a href="president-desk.php"><img src="img/mangement/2025/SUKHBIR_SINGH_PRESIDENT.png" alt="img" /></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-5 col-md-12 col-sm-12">
-              <div class="team-img-box">
-                <a href="team-single.html"
-                  ><img src="img/team/team01.jpg" alt="img"
-                /></a>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </section>
 
     
