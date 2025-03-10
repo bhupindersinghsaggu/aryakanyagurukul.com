02b29f1b74b96065cc9d9834351d124d4a0d589d
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-03-10 14:40:47 +0530

    Update president-message.php

--- akgsss/president-message.php
+++ akgsss/president-message.php
@@ -31,87 +31,29 @@ include('_web/header.php')
 <!-- Blog Standard Start -->
 <div class="section blog-standard-section section-padding">
     <div class="container">
-        <!-- Blog Standard Wrap Start -->
         <div class="blog-standard-wrap">
             <div class="row">
-                <div class="col-lg-8">
-                    <!-- Blog Post Wrap Start -->
+                <div class="col-lg-4">
                     <div class="blog-post-wrap">
-                        <!-- Single Blog Start -->
                         <div class="single-blog single-blog-post">
                             <div class="blog-img">
                                 <a href="blog-details.php"><img src="assets/images/mangement/2025/SUKHBIR_SINGH_PRESIDENT.png" alt=""></a>
                             </div>
-                            <div class="blog-content">
-                                <p>Dear students, Parents and Esteemed Faculty,</p>
-                                <p> It is with great joy and pride that I welcome you to Arya Kanya Gurukul, a place of learning, growth and excellence. At our Gurukul believe in nurturing young minds nith knowledge, discipline and values that shape responsible citizens. Our mission is to empower girls with skills, wisdom and confidence to excel in all walks of life .Remember education is not just about acquiring knowledge but about developing a mindset of curiosity. Believe in yourself, work hard and success will follow. As we move forward let us work together to make Gurukul a place of inspiration and excellence. Wishing you all a year filled with learning, growth and success.</p>
-                                <div class="profile-content">
-                           
-                                    <h4 class="name">Sukhbir Singh</h4>
-                                    <p class="designation"><b>President (Arya Kanya Gurukul Mor Majra,Karnal)</b></p>
-                                </div>
-                            </div>
                         </div>
-                        <!-- Single Blog End -->
-
-
-                        <!-- Single Blog End -->
-
-                        <!-- upstudy Pagination End -->
                     </div>
-                    <!-- Blog Post Wrap Ed -->
                 </div>
-                <div class="col-lg-4">
-                    <!-- Blog Sidebar Start -->
-                    <div class="blog-sidebar">
-                        <!-- Sidebar Widget Start -->
-                        <!-- Sidebar Widget Start -->
-                        <div class="sidebar-widget">
-                            <!-- Widget Title Start -->
-
-                            <!-- Widget Title End -->
-                            <!-- Widget Recent Post Start -->
-
-                            <!-- Widget Recent Post End -->
+                <div class="col-lg-8">
+                    <div class="blog-content">
+                        <p>Dear students, Parents and Esteemed Faculty,</p>
+                        <p> It is with great joy and pride that I welcome you to Arya Kanya Gurukul, a place of learning, growth and excellence. At our Gurukul believe in nurturing young minds nith knowledge, discipline and values that shape responsible citizens. Our mission is to empower girls with skills, wisdom and confidence to excel in all walks of life .Remember education is not just about acquiring knowledge but about developing a mindset of curiosity. Believe in yourself, work hard and success will follow. As we move forward let us work together to make Gurukul a place of inspiration and excellence. Wishing you all a year filled with learning, growth and success.</p>
+                        <div class="profile-content">
+                            <h4 class="name">Sukhbir Singh</h4>
+                            <p class="designation"><b>President (Arya Kanya Gurukul Mor Majra,Karnal)</b></p>
                         </div>
-                        <!-- Sidebar Widget End -->
-
-                        <!-- Sidebar Widget Start -->
-                        <div class="sidebar-widget">
-                            <!-- Widget Title Start -->
-                            <div class="widget-title">
-                                <h3 class="title">More Link</h3>
-                            </div>
-                            <!-- Widget Title End -->
-                            <!-- Widget Category Start -->
-                            <ul class="category">
-                                <li class="cate-item"><a href="about-gurukul.php"><i class="flaticon-next"></i> About Gurukul
-                                    </a></li>
-                                <li class="cate-item"><a href="announcements.php"><i class="flaticon-next"></i> Announcements
-                                    </a></li>
-                                <li class="cate-item"><a href="our-history.php"><i class="flaticon-next"></i> Our-history</a></li>
-                                <li class="cate-item"><a href="president-message.php"><i class="flaticon-next"></i>President's-message
-                                    </a></li>
-                                <li class="cate-item"><a href="principal-message.php"><i class="flaticon-next"></i>Principal's-message
-                                    </a></li>
-                                <li class="cate-item"><a href="achievement.php"><i class="flaticon-next"></i>Achievement
-                                    </a></li>
-                                <li class="cate-item"><a href="toppers.php"><i class="flaticon-next"></i>Toppers
-                                    </a></li>
-
-                            </ul>
-                            <!-- Widget Category End -->
-                        </div>
-                        <!-- Sidebar Widget End -->
-
-                        <!-- Sidebar Widget Start -->
-
                     </div>
-                    <!-- Blog Sidebar End -->
                 </div>
             </div>
         </div>
-        <!-- Blog Standard Wrap End -->
     </div>
 </div>
 <!-- Blog Standard End -->
