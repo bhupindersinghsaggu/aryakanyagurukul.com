36a0d4d31048cc4edaad052b0275932c77ade5e6
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-03-14 18:15:34 +0530

    sedd

--- akgm/governing-body.php
+++ akgm/governing-body.php
@@ -40,7 +40,7 @@ include('_web/header.php');
                         </div>
                     </div>
                     <div class="team-info">
-                        <h4>Sh. Sukhbir Singh Mann</h4>
+                        <h4>Sh. Sukhbir Singh </h4>
                         <p>President</p>
                     </div>
                 </div>
@@ -93,7 +93,7 @@ include('_web/header.php');
                     </div>
                     <div class="team-info">
                         <h4>Sh. Narinder Singh </h4>
-                        <p>Cashier </p>
+                        <p>Treasurerss </p>
                     </div>
                 </div>
             </div>
